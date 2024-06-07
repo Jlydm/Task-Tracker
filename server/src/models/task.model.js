c53f9b174e58816bbd33e98d451f6733ff3fe8c8
Jlydm <juliancito8790@gmail.com>
2024-06-07 16:06:17 -0500

    Volviendo a hacer los modelos...

---
+++ server/src/models/task.model.js
@@ -0,0 +1,37 @@
+import { DataTypes } from "sequelize";
+import TaskDatabase from "../config/db.s";
+import User from "./user.model.js"
+
+const Task = TaskDatabase.define('tasks', {
+  task_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  text: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.UUID,
+    references: {
+      model: User,
+      key: 'user_id',
+    }
+  }
+})
+
+// 1:M
+User.hasMany(Task, {foreignKey: 'user_id'});
+Task.belongTo(User, {foreignKey: 'user_id'});
+
+module.exports = Task;
--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,13 +1,26 @@
-import { DataTypes, Sequelize } from "sequelize";
-import TaskDatabase from "../config/db";
+import { DataTypes } from "sequelize";
+import TaskDatabase from "../config/db.js";
 
-const User = TaskDatabase.define(
-  'users', {
-    user_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    }
-  }
-)
+const User = TaskDatabase.define("users", {
+  user_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
